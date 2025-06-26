d4541232c4e2876dbb443f8d4bed9797624fc775
1F10240130 <s1f102401304@iniad.org>
2025-06-26 11:23:43 +0900

    テストのサンプルを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SampleTestCase(TestCase):
+    def test_sample(self):
+        # This is a sample test case
+        self.assertEqual(1 + 2, 3)# This should fail to demonstrate a failing test
