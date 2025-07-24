bed14b0fade49ea30a1e97ee8e02f207c70f7d85
1F10240055 <s1F102400550@iniad.org>
2025-07-24 23:11:52 +0900

    消去機能の追加

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -61,3 +62,10 @@ def close(request, task_id):
     task.save()
     return redirect(index)
 
+def delete(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404('Task does not exist')
+    task.delete()
+    return redirect(index)
